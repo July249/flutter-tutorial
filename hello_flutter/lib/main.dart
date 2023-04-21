a4581348af0987ce1243f58c0ea1ecd13f61594d
jihyunglee <jhlee7125@gmail.com>
2023-04-22 05:45:17 +0900

    Hello Flutter

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -7,8 +7,11 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // MaterialApp is a convenience widget that wraps a number of widgets
+    // CupertinoApp is a convenience widget that wraps a number of widgets
     return MaterialApp(
       // home: Text('Hello World!'),
+      // Scaffold is a layout for the major Material Components.
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hello Flutter!'),
