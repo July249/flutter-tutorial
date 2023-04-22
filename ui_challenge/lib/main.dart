c406e248efd92e0372f331930d12a2769fac3366
jihyunglee <jhlee7125@gmail.com>
2023-04-22 14:10:08 +0900

    Reusable Widgets - Button

--- ui_challenge/lib/main.dart
+++ ui_challenge/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_challenge/widgets/button.dart';
 
 void main() {
   runApp(const App());
@@ -13,7 +14,9 @@ class App extends StatelessWidget {
       home: Scaffold(
           backgroundColor: const Color(0xFF181818),
           body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -71,25 +74,17 @@ class App extends StatelessWidget {
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFF2B33A),
-                        borderRadius: BorderRadius.circular(45),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 20,
-                          horizontal: 50,
-                        ),
-                        child: Text(
-                          'Transfer',
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
+                  children: const [
+                    Button(
+                      text: 'Transfer',
+                      bgColor: Color(0x0ff1b33b),
+                      textColor: Colors.black,
                     ),
+                    Button(
+                      text: 'Request',
+                      bgColor: Color(0xff1f2123),
+                      textColor: Colors.white,
+                    )
                   ],
                 ),
               ],
