998a73d9953a2ca09845b72e64e8ec5d370da144
jihyunglee <jhlee7125@gmail.com>
2023-04-22 18:17:48 +0900

    make counter by using stateful widget

--- stateful_widgets/lib/main.dart
+++ stateful_widgets/lib/main.dart
@@ -4,16 +4,53 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  void onClicked() {
+    // setState() runs to update the UI by calling build() again
+    setState(() {
+      // Note: counter++ is not necessariliy inside of setState()
+      // But! it is strongly recommended to make it more reaadable!
+      counter++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFFFEFEFE),
         body: Center(
-          child: Text('Hello World'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
